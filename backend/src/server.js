3e117c69cc6da895379656844976f33787e4681a
VDewMin <134048887+VDewMin@users.noreply.github.com>
2025-08-19 12:09:54 +0530

    Initial commit - MERN project

---
+++ backend/src/server.js
@@ -0,0 +1,28 @@
+
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5001
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+}));
+
+//middleware
+app.use(express.json());  //this middleware will parse JSON bodies: req.body
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on port 5001");
+    });
+});
+
+
+
