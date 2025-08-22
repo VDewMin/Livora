b1ea258634a6b7e4b832e6426f5b7387fbf6501c
Kaveesha Sandeepani <kaveeshasandeepani027@gmail.com>
2025-08-22 12:19:41 +0530

    Update ks_parcelRoutes.js

--- backend/src/routes/ks_parcelRoutes.js
+++ backend/src/routes/ks_parcelRoutes.js
@@ -4,16 +4,15 @@ import {
   getAllParcels,
   createParcels,
   updateParcel,
+  getParcelById,
 } from "../controllers/ks_parcelController.js";
 
 const router = express.Router();
 
 router.get("/", getAllParcels);
-
+router.get("/:id", getParcelById);
 router.post("/", createParcels);
-
 router.put("/:id", updateParcel);
-
 router.delete("/:id", deleteParcel);
 
 export default router;
