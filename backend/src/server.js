c6f953e14c55d0b4da3a0a07428558e6b6eef6f5
Sachintha Nirmal <sachinthanirmal658@gmail.com>
2025-08-26 15:10:46 +0530

    update server 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,10 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import sn_paymentRoutes from "./routes/sn_paymentRoutes.js"
 import path from "path";
 import usersRoutes from "./routes/vd_usersRoutes.js";
 import serviceRequestRouter from "./routes/GKServicceRequestRoutes.js";
-import sn_paymentRoutes from "./routes/sn_paymentRoutes.js"
-
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
@@ -21,7 +20,9 @@ app.use(
 //middleware
 app.use(express.json());  //this middleware will parse JSON bodies: req.body
 
+app.use("/api/payments", sn_paymentRoutes)
 app.use("/api/users", usersRoutes);
+app.use("/api/services", serviceRequestRouter);
 
 connectDB().then(() => {
     app.listen(PORT, () => {
