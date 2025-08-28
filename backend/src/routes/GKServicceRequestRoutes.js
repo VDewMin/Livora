dd5f62cac508252e59a36c0b9ea4e1960a4e9a3a
Gagana kalhara <ggaganakalhara08@gmail.com>
2025-08-28 08:50:21 +0530

    hyyy
    update my files

--- backend/src/routes/GKServicceRequestRoutes.js
+++ backend/src/routes/GKServicceRequestRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import{gerAllServices, createServices, updateServices, 
+import{getAllServices, getServicesById, createServices, updateServices, 
     deleteServices } from "../controllers/GKServiceRequestCtrl.js";
 const router = express.Router();
 
-router.get("/", gerAllServices);
+router.get("/", getAllServices);
+router.get("/:id", getServicesById);
 router.post("/", createServices);
 router.put("/:id", updateServices);
 router.delete("/:id", deleteServices);
