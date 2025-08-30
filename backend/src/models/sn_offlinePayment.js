3ffc3f7718e1c3b9ec82e1fb03e2ff690a7ac637
Sachintha Nirmal <sachinthanirmal658@gmail.com>
2025-08-30 13:08:41 +0530

    online gateway done

---
+++ backend/src/models/sn_offlinePayment.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import User from "./vd_user.js";
+
+const offlinePaymentShema = new mongoose.Schema({
+    paymentId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    residentId: {
+        type: String,
+        ref: User,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    amountRent: {
+        type: Number,
+        required: false
+    },
+    amountLaundry: {
+        type: Number,
+        required: false
+    },
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    paymentDate: {
+        type: Date,
+        required: true
+    },
+    slipFile: {
+        data: Buffer,
+        contentType: String
+    },
+    verified: {
+        type: Boolean,
+        default: false
+    },
+    status: {
+        type: String,
+        enum: ["Pending", "Completed", "Failed"],
+        default: "Pending"
+    }
+});
+
+offlinePaymentShema.pre("save", function(next){
+    this.totalAmount = this.amountRent + this.amountLaundry;
+    next();
+});
+
+const OfflinePayment = mongoose.model("OfflinePayment", offlinePaymentShema);
+
+export default OfflinePayment;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,18 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import sn_paymentRoutes from "./routes/sn_paymentRoutes.js"
+import sn_stripeRoutes from "./routes/sn_stripeRoutes.js"
 import path from "path";
 import usersRoutes from "./routes/vd_usersRoutes.js";
 import parcelRoutes from "./routes/ks_parcelRoutes.js"
-
+import Stripe from "stripe"
 import serviceRequestRouter from "./routes/GKServicceRequestRoutes.js";
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 app.use(
     cors({
@@ -27,6 +29,9 @@ app.use("/api/users", usersRoutes);
 app.use("/api/parcels", parcelRoutes);
 app.use("/api/payments", sn_paymentRoutes)
 app.use("/api/services", serviceRequestRouter);
+app.use("/api/checkout", sn_stripeRoutes);
+
+
 
 connectDB().then(() => {
     app.listen(PORT, () => {
